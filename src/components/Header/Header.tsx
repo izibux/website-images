720973c1fb34da539b331480eb2d32707ff5813b
Seu Nome <seu@email.com>
2025-11-08 14:01:54 -0300

    Improve header: add Discord button with original icon, add more nav items

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -26,14 +26,23 @@ const Header = () => {
         {/* Desktop Navigation */}
         <nav className={styles.nav}>
           <a href="#home" className={styles.navLink}>Início</a>
-          <a href="#about" className={styles.navLink}>Sobre</a>
-          <a href="#contact" className={styles.navLink}>Contato</a>
+          <a href="#products" className={styles.navLink}>Pacotes</a>
+          <a href="#how-it-works" className={styles.navLink}>Como Funciona</a>
+          <a href="#faq" className={styles.navLink}>FAQ</a>
         </nav>
 
-        {/* CTA Button */}
-        <button className={styles.ctaButton}>
-          Começar
-        </button>
+        {/* Discord Button */}
+        <a 
+          href="https://discord.gg/seu-servidor" 
+          className={styles.discordButton}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20.317 4.37a19.791 19.791 0 00-4.885-1.515.074.074 0 00-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 00-5.487 0 12.64 12.64 0 00-.617-1.25.077.077 0 00-.079-.037A19.736 19.736 0 003.677 4.37a.07.07 0 00-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 00.031.057 19.9 19.9 0 005.993 3.03.078.078 0 00.084-.028c.462-.63.874-1.295 1.226-1.994a.076.076 0 00-.041-.106 13.107 13.107 0 01-1.872-.892.077.077 0 01-.008-.128 10.2 10.2 0 00.372-.292.074.074 0 01.077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 01.078.01c.12.098.246.198.373.292a.077.077 0 01-.006.127 12.299 12.299 0 01-1.873.892.077.077 0 00-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 00.084.028 19.839 19.839 0 006.002-3.03.077.077 0 00.032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 00-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z" fill="currentColor"/>
+          </svg>
+          <span>Discord</span>
+        </a>
 
         {/* Mobile Menu Button */}
         <button 
@@ -51,9 +60,21 @@ const Header = () => {
       {isMenuOpen && (
         <div className={styles.mobileMenu}>
           <a href="#home" className={styles.mobileNavLink} onClick={toggleMenu}>Início</a>
-          <a href="#about" className={styles.mobileNavLink} onClick={toggleMenu}>Sobre</a>
-          <a href="#contact" className={styles.mobileNavLink} onClick={toggleMenu}>Contato</a>
-          <button className={styles.mobileCtaButton}>Começar</button>
+          <a href="#products" className={styles.mobileNavLink} onClick={toggleMenu}>Pacotes</a>
+          <a href="#how-it-works" className={styles.mobileNavLink} onClick={toggleMenu}>Como Funciona</a>
+          <a href="#faq" className={styles.mobileNavLink} onClick={toggleMenu}>FAQ</a>
+          <a 
+            href="https://discord.gg/seu-servidor" 
+            className={styles.mobileDiscordButton}
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={toggleMenu}
+          >
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M20.317 4.37a19.791 19.791 0 00-4.885-1.515.074.074 0 00-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 00-5.487 0 12.64 12.64 0 00-.617-1.25.077.077 0 00-.079-.037A19.736 19.736 0 003.677 4.37a.07.07 0 00-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 00.031.057 19.9 19.9 0 005.993 3.03.078.078 0 00.084-.028c.462-.63.874-1.295 1.226-1.994a.076.076 0 00-.041-.106 13.107 13.107 0 01-1.872-.892.077.077 0 01-.008-.128 10.2 10.2 0 00.372-.292.074.074 0 01.077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 01.078.01c.12.098.246.198.373.292a.077.077 0 01-.006.127 12.299 12.299 0 01-1.873.892.077.077 0 00-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 00.084.028 19.839 19.839 0 006.002-3.03.077.077 0 00.032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 00-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z" fill="currentColor"/>
+            </svg>
+            Discord
+          </a>
         </div>
       )}
     </header>
